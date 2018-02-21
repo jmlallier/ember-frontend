32ee6e4e5deaaaf1e56a247d140fc1472fc5a772
John-Michael L'Allier <jmlallier@me.com>
2018-02-20 21:16:57 -0500

    add active record adapter and change proxy to listen for rails json api

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,9 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
 	model() {
-		return [{
-			name: 'John-Michael',
-			age: 26
-		}, {name: 'Angelique', age: 30}]
+		return this.store.findAll('skill');
 	}
 });
